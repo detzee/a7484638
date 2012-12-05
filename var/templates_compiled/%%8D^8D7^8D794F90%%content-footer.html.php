f22acce3676002933035b0a7523e17b2b451a5c4
Jason Smith <jason@hypershift.net>
2012-12-05 15:50:55 +0700

    First init

---
+++ var/templates_compiled/%%8D^8D7^8D794F90%%content-footer.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2012-12-03 16:05:51
+         compiled from layout/content-footer.html */ ?>
+<?php if ($this->_tpl_vars['showContentFrame']): ?>
+</div>
+<?php endif; ?>
